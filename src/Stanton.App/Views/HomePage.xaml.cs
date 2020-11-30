719fec1d3d9b2b7d117bd30bdfa3050e26bf9235
Akiya Xiao <akiya.xiao@icloud.com>
2020-11-30 18:01:17 +0800

    add 3d model view

--- src/Stanton.App/Views/HomePage.xaml.cs
+++ src/Stanton.App/Views/HomePage.xaml.cs
@@ -15,15 +15,24 @@ namespace Stanton.App.Views
     /// </summary>
     public sealed partial class HomePage : Page
     {
+        private readonly WebView MyWebView;
         public HomePage()
         {
             this.InitializeComponent();
+            MyWebView = new WebView(WebViewExecutionMode.SeparateProcess)
+            {
+                Source = new Uri("https://sketchfab.com/models/a6af6d1ae2744a55820d00599aca71f2/embed?autostart=1&internal=1&ui_infos=0&ui_snapshots=1&ui_stop=0&ui_watermark=0")
+            };
+            WeaponryTab.Children.Add(MyWebView);
         }
 
         private void GridView_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             var col = Math.Min(Math.Ceiling(ActualWidth / 400), GridView.Items.Count);
             ((ItemsWrapGrid)GridView.ItemsPanelRoot).ItemWidth = e.NewSize.Width / col;
+            MyWebView.Width = e.NewSize.Width - 24;
+            MyWebView.Height = e.NewSize.Width /2;
+
         }
 
         private void Page_LayoutUpdated(object sender, object e)
