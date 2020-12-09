92efa4268900717da61b44882c3363f07aa344b1
Akiya Xiao <akiya.xiao@icloud.com>
2020-12-09 23:52:07 +0800

    Add ship list design 🍹

--- src/Stanton.App/ViewModels/ShipDetailViewModel.cs
+++ src/Stanton.App/ViewModels/ShipDetailViewModel.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Stanton.Common.Entity;
 
 namespace Stanton.App.ViewModels
 {
     class ShipDetailViewModel : ObservableObject
     {
+        public Ship ship;
     }
 }
---
+++ src/Stanton.App/ViewModels/ShipListViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Stanton.Common.Entity;
+using Stanton.App.Services;
+using Stanton.Service;
+
+namespace Stanton.App.ViewModels
+{
+    public class ShipListViewModel : ObservableObject
+    {
+        public ObservableCollection<Ship> Source = new ObservableCollection<Ship>();
+
+        public async Task LoadDataAsync()
+        {
+            Source.Clear();
+            await DataLoadService.LoadShipDataAsync();
+            var data = ShipManager.GetAllShips();
+            foreach(var item in data)
+            {
+                Source.Add(item);
+            }
+        }
+
+        private void OnItemSelected(ItemClickEventArgs args)
+        {
+            
+        }
+    }
+}
