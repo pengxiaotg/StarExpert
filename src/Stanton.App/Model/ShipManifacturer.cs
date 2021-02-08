348321be7e339456eafa27b0562fcef88d1545f6
Akiya Xiao <akiya.xiao@icloud.com>
2021-02-08 17:22:42 +0800

    Add ship list filter function

---
+++ src/Stanton.App/Model/ShipManifacturer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stanton.App.Model
+{
+    public class ShipManifacturer : IEquatable<ShipManifacturer>
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+
+        public bool Equals(ShipManifacturer other)
+        {
+            return Name == other.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
